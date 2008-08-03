1ca0db6ebc3c53af961e25a712ffa04b731690ae
miguelmoreto <miguelmoreto@8c99f430-f94f-0410-9834-4bd18bfc9907>
2008-08-03 15:53:42 +0000

    Interface atualizada. Botões funcionando. Modulo sistema atualizado. Adicionado script para gerar executável com o py2exe.

--- trunk/DlgFT.py
+++ trunk/DlgFT.py
@@ -1,3 +1,4 @@
+# -*- coding: iso-8859-1 -*-
 #Boa:Dialog:Dialog1
 
 import wx
@@ -5,35 +6,66 @@ import wx
 def create(parent):
     return Dialog1(parent)
 
-[wxID_DIALOG1, wxID_DIALOG1PANEL1, 
-] = [wx.NewId() for _init_ctrls in range(2)]
+[wxID_DIALOG1, wxID_DIALOG1BTNCANCEL, wxID_DIALOG1BTNOK, 
+ wxID_DIALOG1TEXTCTRLDEN, wxID_DIALOG1TEXTCTRLNUM, wxID_DIALOG1TEXTDEN, 
+ wxID_DIALOG1TEXTHELP, wxID_DIALOG1TEXTNUM, 
+] = [wx.NewId() for _init_ctrls in range(8)]
 
 class Dialog1(wx.Dialog):
-    def _init_coll_boxSizer1_Items(self, parent):
-        # generated method, don't edit
-
-        parent.AddWindow(self.panel1, 0, border=1, flag=wx.GROW | wx.EXPAND)
-
-    def _init_sizers(self):
-        # generated method, don't edit
-        self.boxSizer1 = wx.BoxSizer(orient=wx.VERTICAL)
-
-        self._init_coll_boxSizer1_Items(self.boxSizer1)
-
-        self.SetSizer(self.boxSizer1)
-
     def _init_ctrls(self, prnt):
         # generated method, don't edit
         wx.Dialog.__init__(self, id=wxID_DIALOG1, name='', parent=prnt,
-              pos=wx.Point(485, 347), size=wx.Size(400, 250),
-              style=wx.DEFAULT_DIALOG_STYLE, title='Dialog1')
-        self.SetClientSize(wx.Size(392, 222))
+              pos=wx.Point(670, 446), size=wx.Size(256, 235),
+              style=wx.DEFAULT_DIALOG_STYLE, title='Par\xe2metros da FT')
+        self.SetClientSize(wx.Size(248, 207))
 
-        self.panel1 = wx.Panel(id=wxID_DIALOG1PANEL1, name='panel1',
-              parent=self, pos=wx.Point(0, 0), size=wx.Size(392, 100),
-              style=wx.TAB_TRAVERSAL)
+        self.btnOk = wx.Button(id=wxID_DIALOG1BTNOK, label='Ok', name='btnOk',
+              parent=self, pos=wx.Point(16, 168), size=wx.Size(80, 30),
+              style=0)
+        self.btnOk.Bind(wx.EVT_BUTTON, self.OnBtnOkButton, id=wxID_DIALOG1BTNOK)
 
-        self._init_sizers()
+        self.btnCancel = wx.Button(id=wx.ID_CANCEL, label='Cancelar',
+              name='btnCancel', parent=self, pos=wx.Point(152, 168),
+              size=wx.Size(80, 30), style=0)
+
+        self.textCtrlDen = wx.TextCtrl(id=wxID_DIALOG1TEXTCTRLDEN,
+              name='textCtrlDen', parent=self, pos=wx.Point(96, 132),
+              size=wx.Size(116, 21), style=0, value='[1, 2, 10]')
+
+        self.textCtrlNum = wx.TextCtrl(id=wxID_DIALOG1TEXTCTRLNUM,
+              name='textCtrlNum', parent=self, pos=wx.Point(96, 96),
+              size=wx.Size(116, 21), style=0, value='[2,10]')
+
+        self.textNum = wx.StaticText(id=wxID_DIALOG1TEXTNUM, label='Numerador:',
+              name='textNum', parent=self, pos=wx.Point(32, 100),
+              size=wx.Size(56, 13), style=0)
+
+        self.textHelp = wx.StaticText(id=wxID_DIALOG1TEXTHELP,
+              label='Preencha os campos abaixo com os coeficientes\ndos polin\xf4mios em fun\xe7\xe3o da vari\xe1vel s que\ndescrevem o numerador e denominador da fun-\n\xe7\xe3o de transfer\xeancia.\nExemplo:\ns^2+2s+10 => [1, 2, 10]',
+              name='textHelp', parent=self, pos=wx.Point(8, 8),
+              size=wx.Size(229, 78), style=0)
+
+        self.textDen = wx.StaticText(id=wxID_DIALOG1TEXTDEN,
+              label='Denominador:', name='textDen', parent=self,
+              pos=wx.Point(21, 136), size=wx.Size(67, 13), style=0)
 
     def __init__(self, parent):
         self._init_ctrls(parent)
+                
+
+    def OnBtnOkButton(self, event):
+        """
+        Evento do botão OK. Lê os valores dos campos de entrada.
+        """
+        self.Num = eval(self.textCtrlNum.GetLineText(0))
+        
+        self.Den = eval(self.textCtrlDen.GetLineText(0))
+        
+        self.EndModal(wx.ID_OK)
+
+    def AtualizaCampos(self,Num,Den):
+    
+        self.textCtrlNum.SetValue(str(Num))
+        self.textCtrlDen.SetValue(str(Den))
+
+
