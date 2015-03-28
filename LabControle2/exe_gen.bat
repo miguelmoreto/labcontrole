bd8b0cf8e5f2f1f6b0d4df26a9679928d66deb65
miguelmoreto <miguelmoreto@8c99f430-f94f-0410-9834-4bd18bfc9907>
2015-03-28 12:53:18 +0000

    Deleting folder LabControle2

--- LabControle2/exe_gen.bat
+++
@@ -1,9 +0,0 @@
-echo Generating binary dist version...
-c:\WinPython-64bit-2.7.6.3\python-2.7.6.amd64\Scripts\pyinstaller.exe -w -D LabControle2.py --hidden-import=scipy.special._ufuncs_cxx
-echo Copying hacked manifest file...
-copy /Y Microsoft.VC90.MFC.manifest .\dist\LabControle2\Microsoft.VC90.MFC.manifest
-echo Manifest updated.
-echo Copying figures..
-copy .\*.svg .\dist\LabControle2\
-mkdir .\dist\LabControle2\images
-copy .\images\*.* .\dist\LabControle2\images\
--- LabControle2/geraui.bat
+++
@@ -1,4 +0,0 @@
-echo MainWindow..
-python "%WINPYDIR%\Lib\site-packages\PyQt4\uic\pyuic.py" -x MainWindow.ui -o MainWindow.py
-rem echo Dialog..
-rem python "%WINPYDIR%\Lib\site-packages\PyQt4\uic\pyuic.py" -x DialogSysInfo.ui -o DialogSysInfo.py
