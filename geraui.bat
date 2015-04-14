f169c180671ff06bba1aaa899f9a77c737b3f9b3
Miguel Moreto <miguelmoreto@gmail.com>
2015-04-14 13:46:37 -0300

    Updated user interface. Added non-linear system block diagram.

--- geraui.bat
+++ geraui.bat
@@ -1,5 +1,5 @@
 echo MainWindow..
-python "%WINPYDIR%\Lib\site-packages\PyQt4\uic\pyuic.py" -x MainWindow.ui -o MainWindow.py
+python "C:\WinPython-64bit-2.7.9.2\python-2.7.9.amd64\Lib\site-packages\PyQt4\uic\pyuic.py" -x MainWindow.ui -o MainWindow.py
 rem echo Dialog..
 rem python "%WINPYDIR%\Lib\site-packages\PyQt4\uic\pyuic.py" -x DialogSysInfo.ui -o DialogSysInfo.py
 pause
