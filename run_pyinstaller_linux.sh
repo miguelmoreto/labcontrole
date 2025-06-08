80690b777790c3c4464b5021d6481f394b6e08d9
Miguel Moreto <miguelmoreto@users.noreply.github.com>
2025-06-08 10:56:26 -0300

    Updated script for generate a binary distribution in Linux.

--- run_pyinstaller_linux.sh
+++ run_pyinstaller_linux.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 pyinstaller --noconfirm \
     --clean --onedir \
+    --contents-directory "." \
+    --name=LabControle3 \
     --add-data="MainWindow.ui:." \
     --add-data="HelpWindow.ui:." \
     --add-data="matplotlibwidget.py:." \
+    --add-data="./help/*:./help" \
+    --add-data="./images/*:./images" \
+    --add-data="./images/mpl_toolbar/*:./images/mpl_toolbar"
     --exclude-module=sqlalchemy \
-    --hidden-import=PyQt5.sip \
+    --hidden-import=numpy.core.multiarray \
     LabControl3.py
-cp libstdc++.so.6 dist/LabControl3/
+#cp libstdc++.so.6 dist/LabControl3/
